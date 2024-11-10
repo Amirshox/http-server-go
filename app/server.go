36cb864fed0831ca2074fce3ac843d9d58a3e632
amirshokh <amirshoxmuxitdinov@gmail.com>
2024-11-11 00:29:39 +0500

    response 200

--- app/server.go
+++ app/server.go
@@ -6,9 +6,6 @@ import (
 	"os"
 )
 
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -17,10 +14,32 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Create a simple HTTP response
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 25\r\n" +
+		"\r\n" +
+		"Hello, this is a response!"
+
+	// Send the response to the client
+	_, err := conn.Write([]byte(response))
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error writing response:", err.Error())
 	}
 }
