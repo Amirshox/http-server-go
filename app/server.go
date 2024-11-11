6ce29a36f1f7ce380a76bf77281aaeb2ce3ed153
amirshokh <amirshoxmuxitdinov@gmail.com>
2024-11-11 16:08:19 +0500

    file content write

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"path/filepath"
@@ -52,7 +53,9 @@ func handleConnection(conn net.Conn) {
 		fmt.Println("Invalid request line:", requestLine)
 		return
 	}
+	method := parts[0]
 	path := parts[1]
+	fmt.Println("Request:", method, path)
 
 	headers := make(map[string]string)
 	for {
@@ -95,8 +98,17 @@ func handleConnection(conn net.Conn) {
 			"Content-Length: " + fmt.Sprint(len(variable)) + "\r\n" +
 			"\r\n" + variable
 	} else if strings.HasPrefix(path, "/files/") {
-		filename := strings.TrimPrefix(path, "/files/")
-		serveFile(conn, filename)
+		switch method {
+		case "GET":
+			filename := strings.TrimPrefix(path, "/files/")
+			serveFile(conn, filename)
+			return
+		case "POST":
+			handleFileUpload(conn, reader, headers, strings.TrimPrefix(path, "/files/"))
+			return
+		default:
+			response = "HTTP/1.1 405 Method Not Allowed\r\n"
+		}
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n" +
 			"Content-Type: text/plain\r\n" +
@@ -150,4 +162,45 @@ func serveFile(conn net.Conn, filename string) {
 		}
 		conn.Write(buffer[:n])
 	}
+}
+
+func handleFileUpload(conn net.Conn, reader *bufio.Reader, headers map[string]string, filename string) {
+	// Ensure Content-Length is provided
+	contentLengthStr, exists := headers["Content-Length"]
+	if !exists {
+		response := "HTTP/1.1 411 Length Required\r\nContent-Type: text/plain\r\nContent-Length: 19\r\n\r\n411 Length Required"
+		conn.Write([]byte(response))
+		return
+	}
+
+	contentLength, err := strconv.Atoi(contentLengthStr)
+	if err != nil || contentLength < 0 {
+		response := "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\n400 Bad Request"
+		conn.Write([]byte(response))
+		return
+	}
+
+	// Read the body
+	body := make([]byte, contentLength)
+	_, err = io.ReadFull(reader, body)
+	if err != nil {
+		fmt.Println("Error reading request body:", err)
+		response := "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\n500 Internal Server Error"
+		conn.Write([]byte(response))
+		return
+	}
+
+	// Write the file
+	filePath := filepath.Join(filesDir, filename)
+	err = os.WriteFile(filePath, body, 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		response := "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\n500 Internal Server Error"
+		conn.Write([]byte(response))
+		return
+	}
+
+	// Respond with 201 Created
+	response := "HTTP/1.1 201 Created\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\n201 Created Successfully"
+	conn.Write([]byte(response))
 }
